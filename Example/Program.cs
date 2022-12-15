36ec09ea5a4774db441d9e82d0ab1c3125a3e5d0
graphite01 <kirito10082279@gmail.com>
2022-12-15 16:48:52 +0900

    first commit

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace Example
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
