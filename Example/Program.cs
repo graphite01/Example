faa5d8996fab6d27d028270d6527016cac2c90ca
graphite01 <kirito10082279@gmail.com>
2022-12-16 16:51:57 +0900

    変数の名前、所持金&名前を表示

--- Example/Program.cs
+++ Example/Program.cs
@@ -7,7 +7,16 @@ namespace Example
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // 変数の宣言
+            int money;   // 所持金を代入する変数
+            string name; // 名前を代入する変数
+
+            // 変数に値を代入する
+            money = 5000;
+            name = "きたむら";
+
+            Console.WriteLine(money); // 所持金を表示
+            Console.WriteLine(name); // 名前を表示
         }
     }
 }
