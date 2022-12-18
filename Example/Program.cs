6ed38c233919586e346dedc15f0df25b037d7707
graphite01 <kirito10082279@gmail.com>
2022-12-18 16:17:41 +0900

    他の数値で計算練習

--- Example/Program.cs
+++ Example/Program.cs
@@ -7,12 +7,12 @@ namespace Example
         static void Main(string[] args)
         {
             // 変数の初期化
-            int money = 15000; // 現在の所持金
-            int salary = 1000; // 時給
-            int hour = 5; // 働いた時間
+            int money = 5000; // 現在の所持金
+            int salary = 800; // 時給
+            int hour = 2; // 働いた時間
 
             // 現在の所持金の合計を計算して表示
-            int sum = money + (salary + 150) * hour;
+            int sum = money + salary * hour;
             Console.WriteLine(sum);
         }
     }
