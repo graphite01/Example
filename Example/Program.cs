cf3b18e3dd3ab2d2923a4c58eb9d3ded08db938a
graphite01 <kirito10082279@gmail.com>
2022-12-18 16:33:33 +0900

    int型の変数を使用して小数点の結果を表示する

--- Example/Program.cs
+++ Example/Program.cs
@@ -6,13 +6,20 @@ namespace Example
     {
         static void Main(string[] args)
         {
-            int math = 80; // 数学の点数
-            int english = 66; // 英語の点数
-            int history = 95; // 歴史の点数
+            // int型の変数を2つ用意
+            int a = 15;
+            int b = 2;
 
-            // 平均値を計算して表示
-            float average = (math + english + history) / 3.0f;
-            Console.WriteLine("平均は" + average + "点");
+            float answer;   // 浮動小数点の結果を代入する変数を用意
+
+            // int型の値に小数点を掛けて計算する場合
+            answer = a * 1.0f / b;
+            Console.WriteLine(answer);
+
+            // 暗黙のキャストでint型をfloat型にして計算する場合
+            float c = a;
+            answer = c / b;
+            Console.WriteLine(answer);
         }
     }
 }
