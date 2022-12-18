4edd5c589cbb8c950fa9f6d7f40f7a1c2133d96e
graphite01 <kirito10082279@gmail.com>
2022-12-18 16:37:50 +0900

    簡単な書き方で計算結果を表示

--- Example/Program.cs
+++ Example/Program.cs
@@ -9,7 +9,7 @@ namespace Example
             int life = 1;
 
             // ライフを「3」だけ増やす
-            life = life + 3;
+            life += 3;
             Console.WriteLine(life);
         }
     }
