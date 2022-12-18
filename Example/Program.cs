d7ba4201cf1c169ab1ddce32f2003c4f373ed943
graphite01 <kirito10082279@gmail.com>
2022-12-18 14:52:00 +0900

    1~5までの和を求めて表示

--- Example/Program.cs
+++ Example/Program.cs
@@ -2,43 +2,15 @@
 
 namespace Example
 {
-
     class Program
     {
         static void Main(string[] args)
         {
-            // 変数の宣言
-            int money;   // 所持金を代入する変数
-            string name; // 名前を代入する変数
-            int answer; // 計算結果
+            int sum; //計算結果を代入する変数
 
-            // 変数に値を代入する
-            money = 5000;
-            name = "きたむら";
-
-
-            Console.WriteLine(money); // 所持金を表示
-            Console.WriteLine(name); // 名前を表示
-
-            // 加算
-            answer = 3 + 4;
-            Console.WriteLine(answer);
-
-            // 減算
-            answer = 12 - 18;
-            Console.WriteLine(answer);
-
-            // 乗算
-            answer = 2 * 7;
-            Console.WriteLine(answer);
-
-            // 除算
-            answer = 18 / 3;
-            Console.WriteLine(answer);
-
-            // 剰余
-            answer = 21 % 5;
-            Console.WriteLine(answer);
+            // 1~5までの和を求めて表示
+            sum = 1 + 2 + 3 + 4 + 5;
+            Console.WriteLine(sum);
         }
     }
 }
