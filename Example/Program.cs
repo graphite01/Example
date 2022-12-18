66b90df38b13a1fafaf171a0213b24fab31dbd6b
graphite01 <kirito10082279@gmail.com>
2022-12-18 16:36:34 +0900

    変数の値を増やして表示する

--- Example/Program.cs
+++ Example/Program.cs
@@ -6,20 +6,11 @@ namespace Example
     {
         static void Main(string[] args)
         {
-            // int型の変数を2つ用意
-            int a = 15;
-            int b = 2;
+            int life = 1;
 
-            float answer;   // 浮動小数点の結果を代入する変数を用意
-
-            // int型の値に小数点を掛けて計算する場合
-            answer = a * 1.0f / b;
-            Console.WriteLine(answer);
-
-            // 暗黙のキャストでint型をfloat型にして計算する場合
-            float c = a;
-            answer = c / b;
-            Console.WriteLine(answer);
+            // ライフを「3」だけ増やす
+            life = life + 3;
+            Console.WriteLine(life);
         }
     }
 }
