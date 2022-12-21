4f56c50dc4a070269a9a9f9ceb085c08f362a51d
graphite01 <kirito10082279@gmail.com>
2022-12-21 16:57:43 +0900

    複数の条件式、falseの場合の表示

--- Example/Program.cs
+++ Example/Program.cs
@@ -6,7 +6,7 @@ namespace Example
     {
         static void Main(string[] args)
         {
-            int player1PosX = 2; // プレイヤ1の位置
+            int player1PosX = 1; // プレイヤ1の位置
             int player2PosX = 6; // プレイヤ2の位置
 
             // プレイヤ1の位置が「2」かつプレイヤ2の位置が「6」の場合に仕掛けを解除
