630b0038e5f4b4d351eb404a5beaba78c9344e90
graphite01 <kirito10082279@gmail.com>
2022-12-21 16:54:24 +0900

    複数の条件式がtrueの際の表示

--- Example/Program.cs
+++ Example/Program.cs
@@ -10,12 +10,9 @@ namespace Example
             int player2PosX = 6; // プレイヤ2の位置
 
             // プレイヤ1の位置が「2」かつプレイヤ2の位置が「6」の場合に仕掛けを解除
-            if (player1PosX == 2)
+            if (player1PosX == 2 && player2PosX == 6)
             {
-                if (player2PosX == 6)
-                {
-                    Console.WriteLine("仕掛け解除");
-                }
+                Console.WriteLine("仕掛け解除");
             }
         }
     }
