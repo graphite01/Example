2ccd99998b908743e9a95bf802638d57b1ccefe2
graphite01 <kirito10082279@gmail.com>
2022-12-21 17:03:53 +0900

    プレイヤ１とプレイヤ２が左右に入れ替わってもtrueの場合の表示

--- Example/Program.cs
+++ Example/Program.cs
@@ -9,8 +9,9 @@ namespace Example
             int player1PosX = 2; // プレイヤ1の位置
             int player2PosX = 6; // プレイヤ2の位置
 
-            // プレイヤ1の位置が「2」またはプレイヤ2の位置が「6」の場合に仕掛けを解除
-            if (player1PosX == 2 || player2PosX == 6)
+            // プレイヤ1の位置が「2」かつプレイヤ2の位置が「6」の場合、
+            // またはプレイヤ1の位置が「6」かつプレイヤ2の位置が「2」の場合に仕掛けを解除
+            if ((player1PosX == 2 && player2PosX == 6) || (player1PosX == 6 && player2PosX == 2))
             {
                 Console.WriteLine("仕掛け解除");
             }
