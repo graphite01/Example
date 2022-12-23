7d31d1931e90bb69860a338c00881d2f863335ec
graphite01 <kirito10082279@gmail.com>
2022-12-23 15:12:01 +0900

    43kg以下の体重だけを表示する

--- Example/Program.cs
+++ Example/Program.cs
@@ -6,20 +6,17 @@ namespace Example
     {
         static void Main(string[] args)
         {
-            float[] weights = { 41.2f, 42.5f, 44.9f, 43.2f, 45.1f,
-                                43.2f, 42.7f };
+            float[] weights = { 41.2f, 42.5f, 44.9f, 43.2f,
+                                45.1f, 43.2f, 42.7f };
 
-            float sum = 0.0f; // 1週間の体重の合計を入れる変数
-
-            // 1週間の泰淳の合計値を求める
+            // 43kg以下の値だけを表示する
             for (int i = 0; i < weights.Length; i++)
             {
-                sum += weights[i];
+                if (weights[i] <= 43)
+                {
+                    Console.WriteLine(weights[i]);
+                }
             }
-
-            // 1週間の体重の平均値を求める
-            float average = sum / weights.Length;
-            Console.WriteLine("平均値は" + average + "です");
         }
     }
 }
