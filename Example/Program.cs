110578cce865b60d9d0ac8da0086dc34c8ba8efd
graphite01 <kirito10082279@gmail.com>
2022-12-23 14:42:49 +0900

    配列を使用したコード

--- Example/Program.cs
+++ Example/Program.cs
@@ -6,27 +6,23 @@ namespace Example
     {
         static void Main(string[] args)
         {
-            int sum = 0; // 合計値を保持する変数
-            int num = 1;
+            float[] weights;
+            weights = new float[7];
 
-            while (true)
+            // 配列の要素に値を代入する
+            weights[0] = 41.2f;
+            weights[1] = 42.5f;
+            weights[2] = 44.9f;
+            weights[3] = 43.2f;
+            weights[4] = 45.1f;
+            weights[5] = 43.2f;
+            weights[6] = 42.7f;
+
+            // 配列の値を全て表示する
+            for (int i = 0; i < 7; i++)
             {
-                // 合計値にnumの値を加算する
-                sum += num;
-
-                // 現在の合計値を表示
-                Console.WriteLine(sum);
-
-                // 合計値が500を超えたらwhile文を抜ける
-                if (sum >= 500)
-                {
-                    break;
-                }
-
-                // numの値を1増やす
-                num++;
+                Console.WriteLine(weights[i]);
             }
-            Console.WriteLine("計算終了");
         }
     }
 }
