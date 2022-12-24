2ba3e972f52a9a937eb74758d5c1959f2b10a35e
graphite01 <kirito10082279@gmail.com>
2022-12-24 18:15:56 +0900

    引数と戻り値のあるメソッド1

--- Example/Program.cs
+++ Example/Program.cs
@@ -4,30 +4,40 @@ namespace Example
 {
     class Program
     {
-        // 引数に商品名を受け取り、
-        // 商品名に応じた値段を表示するメソッド
-        static void Shop(string itemName)
+        // 商品名を引数に取り、
+        // 対応する商品の値段を返すメソッド
+        static int Shop(string itemName)
         {
             Console.WriteLine("いらっしゃいませ!");
+            int price = 0;
 
             if (itemName == "薬草")
             {
                 Console.WriteLine(itemName + "は100円です");
+                price = 100;
             }
             else if (itemName == "棍棒")
             {
                 Console.WriteLine(itemName + "は1500円です");
+                price = 1500;
             }
             else
             {
                 Console.WriteLine("売り切れです");
+                price = 0;
             }
+
+            // 引数に対応する商品の値段を返す
+            return price;
         }
 
         static void Main(string[] args)
         {
-            // 「薬草」を引数に渡してShopメソッドを呼び出す
-            Shop("");
+            int money = 2500;
+            Console.WriteLine("所持金は" + money + "円です");
+            int price = Shop("薬草");
+            money -= price;
+            Console.WriteLine("所持金は" + money + "円です");
         }
     }
 }
