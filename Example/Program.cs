b640b30aa6ccb0c22c9dd91b6c82d25302fdfcf1
graphite01 <kirito10082279@gmail.com>
2022-12-24 17:52:41 +0900

    引数ありのメソッド１

--- Example/Program.cs
+++ Example/Program.cs
@@ -4,16 +4,30 @@ namespace Example
 {
     class Program
     {
-        // 「ありがとうございました!」と表示するメソッドを定義
-        static void Thankyou()
+        // 引数に商品名を受け取り、
+        // 商品名に応じた値段を表示するメソッド
+        static void Shop(string itemName)
         {
-            Console.WriteLine("ありがとうございました!");
+            Console.WriteLine("いらっしゃいませ!");
+
+            if (itemName == "薬草")
+            {
+                Console.WriteLine(itemName + "は100円です");
+            }
+            else if (itemName == "棍棒")
+            {
+                Console.WriteLine(itemName + "は1500円です");
+            }
+            else
+            {
+                Console.WriteLine("売り切れです");
+            }
         }
 
         static void Main(string[] args)
         {
-            // Thankyouメソッドを呼び出す
-            Thankyou();
+            // 「薬草」を引数に渡してShopメソッドを呼び出す
+            Shop("薬草");
         }
     }
 }
