deed120bdfb94ed7eddc9c59c7c5036d17e784ed
graphite01 <kirito10082279@gmail.com>
2022-12-24 17:08:34 +0900

    文字列を表示するメソッド1

--- Example/Program.cs
+++ Example/Program.cs
@@ -4,19 +4,16 @@ namespace Example
 {
     class Program
     {
+        // 「いらっしゃいませ!」と表示するメソッドを定義
+        static void Shop()
+        {
+            Console.WriteLine("いらっしゃいませ!");
+        }
+
         static void Main(string[] args)
         {
-            float[] weights = { 41.2f, 42.5f, 44.9f, 43.2f,
-                                45.1f, 43.2f, 42.7f };
-
-            // 43kg以下の値だけを表示する
-            for (int i = 0; i < weights.Length; i++)
-            {
-                if (weights[i] <= 43)
-                {
-                    Console.WriteLine(weights[i]);
-                }
-            }
+            // Shopメソッドを呼び出す
+            Shop();
         }
     }
 }
