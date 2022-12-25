67bbca184f644e695ae09919df5b80d3b86dd991
graphite01 <kirito10082279@gmail.com>
2022-12-25 16:54:51 +0900

    偶数か奇数か判断する

--- Example/Program.cs
+++ Example/Program.cs
@@ -4,40 +4,23 @@ namespace Example
 {
     class Program
     {
-        // 商品名を引数に取り、
-        // 対応する商品の値段を返すメソッド
-        static int Shop(string itemName)
+        // 引数の値が偶数か奇数化を表示するメソッド
+        static void ShowEve0r0dd (int num)
         {
-            Console.WriteLine("いらっしゃいませ!");
-            int price = 0;
-
-            if (itemName == "薬草")
+            if ((num % 2) == 0)
             {
-                Console.WriteLine(itemName + "は100円です");
-                price = 100;
-            }
-            else if (itemName == "棍棒")
-            {
-                Console.WriteLine(itemName + "は1500円です");
-                price = 1500;
+                Console.WriteLine("偶数です");
             }
             else
             {
-                Console.WriteLine("売り切れです");
-                price = 0;
+                Console.WriteLine("奇数です");
             }
-
-            // 引数に対応する商品の値段を返す
-            return price;
         }
 
         static void Main(string[] args)
         {
-            int money = 2500;
-            Console.WriteLine("所持金は" + money + "円です");
-            int price = Shop("薬草");
-            money -= price;
-            Console.WriteLine("所持金は" + money + "円です");
+            // ShowEve0r0ddメソッドを呼び出す
+            ShowEve0r0dd(3);
         }
     }
 }
